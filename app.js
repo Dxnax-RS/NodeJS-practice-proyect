a03d2b7709da14737c77449c07397605a0231cbe
Daniel <druizsalzar@gmail.com>
2022-05-16 12:15:37 -0500

    ExpressJS added

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes);
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next();
+});
 
-server.listen(3000);
+app.use((req, res, mext) => {
+    console.log('In another middleware');
+    res.send('<h1>Hellow</h1>');
+});
+
+app.listen(3000);
