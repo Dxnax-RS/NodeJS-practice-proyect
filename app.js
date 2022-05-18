75e844dc7e2761b69d75e34c2148d16c3e6caf01
Daniel <druizsalzar@gmail.com>
2022-05-18 12:41:20 -0500

    Section 5 of the course finished
    Added static files functionality, the page code was separated in html files, added css and pathing

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next();
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use((req, res, mext) => {
-    console.log('In another middleware');
-    res.send('<h1>Hellow</h1>');
+//Serverside middlewares
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Aplication paths middleware
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+//Responce error middleware
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
