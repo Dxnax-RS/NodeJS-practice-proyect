82440ade5c543c12b6cc9cee1ffdf08e3f6e71ee
Daniel <druizsalzar@gmail.com>
2022-05-23 17:02:39 -0500

    Handlebars test
    All html test pages converted to handlebars to review its functionality

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs({ extname: '.hbs', layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -21,7 +23,7 @@ app.use(shopRoutes);
 
 //Responce error middleware
 app.use((req, res, next) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,14 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    const products = adminData.products
-    res.render('shop', {prods: products, docTitle: 'shop'});
+    const products = adminData.products;
+    res.render('shop', {
+        prods: products, 
+        pageTitle: 'shop', 
+        path: '/', 
+        activeShop: true,
+        productCSS: true
+    });
 });
 
 
